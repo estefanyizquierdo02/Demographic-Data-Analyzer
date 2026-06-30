0b29abb79b8ca2efc54e01f4c82aaec12f106a49
Estefany Izquierdo <estefany.izquierdo.02@gmail.com>
2026-06-29 23:54:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from demographic_data_analyzer import calculate_demographic_data
+from unittest import main
+
+# Esto ejecuta tu función
+calculate_demographic_data(print_data=True)
+
+# Esto ejecuta los tests
+main(module='test_module', exit=False)
